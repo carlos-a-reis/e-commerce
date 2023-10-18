62917e079300b081ff51509fed0796353761d1dc
carlos <carlos.aug.dosreis@gmail.com>
2023-10-18 12:28:16 -0300

    feat: adiciona a estrutura do header da página

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBars,
+  faMagnifyingGlass,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons'
+
+export default function Header() {
+  return (
+    <header className="flex justify-center items-center w-screen h-12 bg-white border-b border-mainGray sm:h-15">
+      <div className="flex justify-between w-85 md:w-screen lg:w-324">
+        <div className="flex justify-between items-center h-9 w-17 md:h-12 md:w-48 md:ml-3 lg:ml-0">
+          <FontAwesomeIcon icon={faBars} className="w-6 text-mainBlue md:w-7" />
+          <div className="bg-mainBlue w-9 h-9 md:w-35 md:h-12"></div>
+        </div>
+        <form className="flex items-center ml-6 md:m-0">
+          <input
+            type="text"
+            className="w-36 h-8 border rounded-s-md border-mainBlue placeholder-textGray text-xs text-center md:w-133 md:h-8 md:border-2 md:text-base"
+            placeholder="Search"
+          />
+          <button
+            type="submit"
+            className="flex justify-center items-center w-8 h-8 border rounded-e-md bg-mainBlue border-mainBlue md:w-12"
+          >
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="w-3 h-3 text-white md:w-4 md:h-4"
+            />
+          </button>
+        </form>
+        <div className="flex justify-end items-center h-9 w-17 md:justify-center md:h-12 md:w-48">
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="w-7 text-mainBlue md:w-8"
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,26 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '420px',
+      md: '768px',
+      lg: '1296px',
+    },
     extend: {
+      spacing: {
+        '15': '60px',
+        '17': '72px',
+        '35': '140px',
+        '85': '340px',
+        '133': '532px',
+        '324': '1296px',
+      },
       colors: {
         mainBlue: '#2E5DD4',
         mainGray: '#D9D9D9',
-        yellowStar: '#E9B824'
-      }
+        textGray: '#999999',
+        yellowStar: '#E9B824',
+      },
     },
   },
   plugins: [],
