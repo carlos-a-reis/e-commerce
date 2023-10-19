0325f69e0637361d11c909ba4288711b909b034b
carlos <carlos.aug.dosreis@gmail.com>
2023-10-19 12:37:48 -0300

    feat: cria o menu de categorias da aplicação

---
+++ src/components/CategoryMenu.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+type CategoryProps = {
+  categories: string[]
+}
+
+export function CategoryMenu(props: CategoryProps) {
+  const [showCategoryMenu, setShowCategoryMenu] = useState(false)
+
+  const handleClick = () => {
+    setShowCategoryMenu(!showCategoryMenu)
+  }
+
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={faBars}
+        className="w-6 text-mainBlue md:w-7"
+        onClick={handleClick}
+      />
+      {showCategoryMenu && (
+        <>
+          <aside className="w-75 h-screen left-0 top-0 z-10 pt-6 pl-8 overflow-auto fixed bg-white md:w-82">
+            <h1 className=" text-2xl mb-10 font-bold">All Categories</h1>
+            <ul className="space-y-8 pb-6">
+              {props.categories.map((category) => {
+                let formattedCategory =
+                  category.charAt(0).toUpperCase() + category.slice(1)
+                if (formattedCategory.includes('-')) {
+                  formattedCategory = formattedCategory.replace('-', ' ')
+                }
+                return (
+                  <li className="text-xl" key={category}>
+                    <Link href={`/category/${category}`}>
+                      {formattedCategory}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </aside>
+          <div
+            className="w-screen h-screen z-0 fixed bg-black opacity-60"
+            onClick={handleClick}
+          ></div>
+        </>
+      )}
+    </>
+  )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
+import { CategoryMenu } from './CategoryMenu'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faBars,
   faMagnifyingGlass,
   faCartShopping,
 } from '@fortawesome/free-solid-svg-icons'
 
-export default function Header() {
+export default async function Header() {
+  const response = await fetch('https://dummyjson.com/products/categories')
+  const categories = await response.json()
+
   return (
     <header className="flex justify-center items-center w-screen h-12 bg-white border-b border-mainGray sm:h-15">
       <div className="flex justify-between w-85 md:w-screen lg:w-324">
         <div className="flex justify-between items-center h-9 w-17 md:h-12 md:w-48 md:ml-3 lg:ml-0">
-          <FontAwesomeIcon icon={faBars} className="w-6 text-mainBlue md:w-7" />
+          <CategoryMenu categories={categories} />
           <div className="bg-mainBlue w-9 h-9 md:w-35 md:h-12"></div>
         </div>
         <form className="flex items-center ml-6 md:m-0">
