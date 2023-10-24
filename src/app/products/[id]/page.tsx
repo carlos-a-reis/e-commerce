0075d46824ce83eb8a76af12984fc9da5fe276f4
carlos <carlos.aug.dosreis@gmail.com>
2023-10-24 12:08:36 -0300

    feat: cria a página de detalhes do produto

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
 type ProductProps = {
   params: {
     id: string
@@ -8,5 +12,30 @@ export default async function Product({ params }: ProductProps) {
   const response = await fetch(`https://dummyjson.com/products/${params.id}`)
   const product = await response.json()
 
-  return <p>{product.images[0]}</p>
+  return (
+    <main>
+      <Image
+        width={390}
+        height={390}
+        src={product.thumbnail}
+        alt={product.title}
+        className="h-96 object-cover"
+      />
+      <div className="flex flex-col gap-5 p-5">
+        <h2 className="text-3xl">{product.title}</h2>
+        <div className="flex items-center gap-2.5 divide-textGray">
+          <div className="flex gap-1">
+            <FontAwesomeIcon icon={faStar} className="w-4 text-yellowStar" />
+            <span>{product.rating}</span>
+          </div>
+          <h3 className="border-l border-textGray pl-2.5">{product.brand}</h3>
+          <h3 className="border-l border-textGray pl-2.5">{`Stock: ${product.stock}`}</h3>
+        </div>
+        <h1 className="text-4xl font-semibold text-mainBlue">
+          {`$${product.price.toFixed(2)}`}
+        </h1>
+        <p className="text-textGray">{product.description}</p>
+      </div>
+    </main>
+  )
 }
