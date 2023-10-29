bc919254fd470175f9c6a9826918a9e6d3a6319b
carlos <carlos.aug.dosreis@gmail.com>
2023-10-29 14:41:37 -0300

    feat: cria uma lista que irá renderizar os produtos filtrados

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import ProductCard, { ProductInfo } from './ProductCard'
 
 type ProductListProps = {
   products: ProductInfo[]
 }
 
-export default function ProductList({ products }: ProductListProps) {
+export default function FilteredProductList({ products }: ProductListProps) {
   return (
     <ul className="flex flex-wrap justify-center gap-mbs mt-8 md:w-324 md:gap-3">
       {products.map((product: ProductInfo) => (
