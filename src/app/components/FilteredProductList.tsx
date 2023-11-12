7252e118e98bad0a2d4532b3585b40f4af3b395e
carlos-a-reis <carlos.aug.dosreis@gmail.com>
2023-11-12 16:13:16 -0300

    feat: adiciona a função que ordena os produtos que são renderizados

--- src/app/components/FilteredProductList.tsx
+++ src/app/components/FilteredProductList.tsx
@@ -9,17 +9,17 @@ type ProductListProps = {
 }
 
 export default function FilteredProductList({ products }: ProductListProps) {
-  const [filteredProducts, setFilteredProducts] = useState(products)
+  const [filteredProducts, setFilteredProducts] = useState<ProductInfo[]>()
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    filterByPrice(products)
+    filterProducts(products)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [products])
 
   const filterByPrice = (products: ProductInfo[]) => {
-    const maxPrice = Number(searchParams.get('max_price'))
-    const minPrice = Number(searchParams.get('min_price'))
+    const maxPrice = Number(searchParams.get('max_price')) || 0
+    const minPrice = Number(searchParams.get('min_price')) || 0
 
     let filtered = products
 
@@ -30,14 +30,63 @@ export default function FilteredProductList({ products }: ProductListProps) {
       filtered = filtered.filter((product) => product.price >= minPrice)
     }
 
-    setFilteredProducts(filtered)
+    return filtered
+  }
+
+  const sortProducts = (
+    products: ProductInfo[],
+    key: keyof ProductInfo,
+    reverseOrder = false,
+  ) => {
+    const productsToOrder = products.slice()
+    const orderParam = reverseOrder ? -1 : 1
+
+    return productsToOrder.sort((a, b) => {
+      const valueA = a[key]
+      const valueB = b[key]
+
+      if (valueA < valueB) {
+        return -1 * orderParam
+      } else if (valueA > valueB) {
+        return 1 * orderParam
+      } else {
+        return 0
+      }
+    })
+  }
+
+  const filterByOrder = (products: ProductInfo[]) => {
+    const order = searchParams.get('order_by')
+
+    if (order !== undefined && order !== null) {
+      const rising = order.includes('rising')
+      const decrasing = order.includes('decrasing')
+      const price = order.includes('price')
+      const rating = order.includes('rating')
+
+      if ((rising || decrasing) && (price || rating)) {
+        const orderBy = price ? 'price' : 'rating'
+        console.log(decrasing)
+        return sortProducts(products, orderBy, decrasing)
+      }
+    }
+
+    return products
+  }
+
+  const filterProducts = (products: ProductInfo[]) => {
+    const priceFilter = filterByPrice(products)
+    const orderFilter = filterByOrder(priceFilter)
+
+    setFilteredProducts(orderFilter)
   }
 
   return (
     <ul className="flex flex-wrap justify-center gap-mbs mt-8 md:w-324 md:gap-3">
-      {filteredProducts.map((product: ProductInfo) => (
-        <ProductCard productInfo={product} key={product.id} />
-      ))}
+      {filteredProducts &&
+        filteredProducts.map((product: ProductInfo) => (
+          <ProductCard productInfo={product} key={product.id} />
+        ))}
     </ul>
   )
 }
